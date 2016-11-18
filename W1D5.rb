eb0ba336c6f6442dbaf8eeb96385be757a73c26a
Joe Thomas <Joe@Joes-MacBook-Pro.local>
2016-11-18 06:37:16 -0800

    Create Map class

--- W1D5.rb
+++ W1D5.rb
@@ -21,7 +21,7 @@ class Stack
   attr_reader :ivar
 end
 
-def Queue
+class Queue
   def initialize
     @ivar = []
   end
@@ -36,6 +36,37 @@ def Queue
 
   def show
     p ivar
+  end
+
+  private
+
+  attr_reader :ivar
+end
+
+class Map
+  def initialize
+    @ivar = []
+  end
+
+  def assign(key, value)
+    entry_exists = ivar.find { |pair| pair[0] == key }
+    if entry_exists
+      entry_exists[1] = value
+    else
+      ivar << [key, value]
+    end
+  end
+
+  def lookup(key)
+    ivar.find { |pair| pair[0] == key }[1]
+  end
+
+  def remove(key)
+    ivar.select! { |pair| pair[0] == key }
+  end
+
+  def show
+    ivar.each { |pair| p "#{pair[0]} => #{pair[1]}" } 
   end
 
   private
